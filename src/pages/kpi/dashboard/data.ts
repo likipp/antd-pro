44e47a26fe472e85f0eaf03a0a9593d418023fb5
Administrator <likipp@163.com>
2020-11-20 16:58:49 +0800

    修改KPI报表视图

--- src/pages/kpi/dashboard/data.ts
+++ src/pages/kpi/dashboard/data.ts
@@ -3,11 +3,19 @@ export interface TableListItem {
   uuid?: string;
   deptName?: string;
   kpiName: string;
-  uLimit?: number;
-  tValue?: number;
-  rValue?: { [key: string]: number };
+  uLimit?: string;
+  tValue?: string;
   unit?: string;
-  '2020-08'?: number;
-  '2020-09'?: number;
-  '2020-10'?: number;
+  '2020-01'?: string;
+  '2020-02'?: string;
+  '2020-03'?: string;
+  '2020-04'?: string;
+  '2020-05'?: string;
+  '2020-06'?: string;
+  '2020-07'?: string;
+  '2020-08'?: string;
+  '2020-09'?: string;
+  '2020-10'?: string;
+  '2020-11'?: string;
+  '2020-12'?: string;
 }
--- src/pages/kpi/dashboard/index.tsx
+++ src/pages/kpi/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { TableListItem } from '@/pages/kpi/dashboard/data';
+import { Tag } from 'antd';
 import ProTable, { ProColumns } from '@ant-design/pro-table';
+
 // import {queryKPIData} from "@/pages/kpi/dashboard/service";
 
 const TableList: React.FC = () => {
@@ -8,12 +10,20 @@ const TableList: React.FC = () => {
     {
       key: 1,
       kpiName: '测试1',
-      tValue: 70,
-      rValue: {
-        '2020-08': 100,
-        '2020-09': 30,
-        '2020-10': 40,
-      },
+      tValue: '70',
+      uLimit: '30',
+      '2020-01': '30',
+      '2020-02': '30',
+      '2020-03': '40',
+      '2020-04': '100',
+      '2020-05': '30',
+      '2020-06': '40',
+      '2020-07': '40',
+      '2020-08': '100',
+      '2020-09': '30',
+      '2020-10': '100',
+      '2020-11': '30',
+      '2020-12': '20',
     },
     {
       key: 2,
@@ -25,22 +35,6 @@ const TableList: React.FC = () => {
     },
   ]);
   // const [loading, setLoading] = useState(true);
-
-  // for (let i = 0; i < 5; i += 1) {
-  //   dataSource.push({
-  //     key: 1,
-  //     kpiName: "测试1"
-  //   },
-  //     {
-  //       key: 2,
-  //       kpiName: "测试2"
-  //     },
-  //     {
-  //       key: 3,
-  //       kpiName: "测试3"
-  //     },
-  //     )
-  // }
 
   const columns: ProColumns<TableListItem>[] = [
     {
@@ -54,12 +48,12 @@ const TableList: React.FC = () => {
       dataIndex: 'kpiName',
     },
     {
-      title: '2020-08',
-      dataIndex: '2020-08',
+      title: '目标值',
+      dataIndex: 'tValue',
     },
     {
-      title: '2020-09',
-      dataIndex: '2020-09',
+      title: '下限值',
+      dataIndex: 'uLimit',
     },
   ];
 
@@ -72,11 +66,54 @@ const TableList: React.FC = () => {
   //     setLoading(false);
   //   });
   // }, []);
+
+  let i;
+  let a;
+  for (i = 1; i <= 12; i += 1) {
+    if (i < 10) {
+      a = {
+        title: `2020-0${i}`,
+        dataIndex: `2020-0${i}`,
+        render: (value: any, record: any) => {
+          if (value !== undefined && value !== null && value !== '-') {
+            if (parseInt(value as string, 10) < parseInt(record.uLimit as string, 10)) {
+              return <Tag color="#f5222d">{value}</Tag>;
+            }
+            if (parseInt(value as string, 10) > parseInt(record.tValue as string, 10)) {
+              return <Tag color="#52c41a">{value}</Tag>;
+            }
+            return <Tag color="#faad14">{value}</Tag>;
+          }
+          return <Tag color="#d9d9d9">空</Tag>;
+        },
+      };
+      columns.push(a);
+    } else {
+      a = {
+        title: `2020-${i}`,
+        dataIndex: `2020-${i}`,
+        render: (value: any, record: any) => {
+          if (value !== undefined && value !== null && value !== '-') {
+            if (parseInt(value as string, 10) < parseInt(record.uLimit as string, 10)) {
+              return <Tag color="#f5222d">{value}</Tag>;
+            }
+            if (parseInt(value as string, 10) > parseInt(record.tValue as string, 10)) {
+              return <Tag color="#52c41a">{value}</Tag>;
+            }
+            return <Tag color="#faad14">{value}</Tag>;
+          }
+          return <Tag color="#d9d9d9">空</Tag>;
+        },
+      };
+      columns.push(a);
+    }
+  }
+
   return (
     <ProTable<TableListItem>
       columns={columns}
       dataSource={dataSource}
-      rowKey="kpi"
+      rowKey="kpiName"
       // loading={loading}
     />
   );
