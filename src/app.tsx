48d575ca174c8026e6f082259c7eb0e66f7d0fe7
xiaom.liu <likipp@163.com>
2021-07-20 15:30:11 +0800

    修改异常处理时notification将后端返回的错误信息用于description

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,33 @@
 // import React from 'react';
-import type {Settings as LayoutSettings} from '@ant-design/pro-layout';
-import {message, notification} from 'antd';
-import type {RequestConfig} from 'umi';
-import {history, Link} from 'umi';
+import type { Settings as LayoutSettings } from '@ant-design/pro-layout';
+import { message, notification } from 'antd';
+import type { RequestConfig } from 'umi';
+import { history, Link } from 'umi';
 // import RightContent from '@/components/RightContent';
 // import Footer from '@/components/Footer';
-import type {RequestOptionsInit, ResponseError} from 'umi-request';
-import {queryCurrent} from './services/user';
+import type { RequestOptionsInit, ResponseError } from 'umi-request';
+import { queryCurrent } from './services/user';
 // import defaultSettings from '../config/defaultSettings';
-import type {RunTimeLayoutConfig} from '@@/plugin-layout/layoutExports';
-import {getMenus} from '@/pages/base/user/service';
+import type { RunTimeLayoutConfig } from '@@/plugin-layout/layoutExports';
+import { getMenus } from '@/pages/base/user/service';
 import fixMenuStruct from '@/utils/fixMenuStruct';
 // Cannot find module '/config/defaultRoutes' or its corresponding type declarations.
-import routes from '../config/defaultRoutes'
+import routes from '../config/defaultRoutes';
 
 const loginPath = '/user/login';
 
 export async function getInitialState(): Promise<{
-  name?: string,
-  avatar?: string,
+  name?: string;
+  avatar?: string;
   settings?: Partial<LayoutSettings>;
   currentUser?: API.CurrentUser;
   fetchUserInfo?: () => Promise<API.CurrentUser | undefined>;
 }> {
-
   const fetchUserInfo = async () => {
     try {
       // eslint-disable-next-line @typescript-eslint/no-shadow
       return await queryCurrent().then((res) => {
-        return res.data
+        return res.data;
       });
     } catch (error) {
       history.push(loginPath);
@@ -38,8 +37,8 @@ export async function getInitialState(): Promise<{
   // 如果是登录页面，不执行
   if (history.location.pathname !== loginPath) {
     const currentUser = await fetchUserInfo();
-    const name = currentUser?.nickname
-    const avatar = 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
+    const name = currentUser?.nickname;
+    const avatar = 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png';
     return {
       name,
       avatar,
@@ -109,44 +108,46 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
     //   }
     // },
     logout: () => {
-      localStorage.removeItem("token")
-      message.success("退出登录成功,即将跳转到登录页")
-      history.push('/')
-    }
+      localStorage.removeItem('token');
+      message.success('退出登录成功,即将跳转到登录页');
+      history.push('/');
+    },
   };
 };
 
-const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
-};
+// const codeMessage = {
+//   200: '服务器成功返回请求的数据。',
+//   201: '新建或修改数据成功。',
+//   202: '一个请求已经进入后台排队（异步任务）。',
+//   204: '删除数据成功。',
+//   400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+//   401: '用户没有权限（令牌、用户名、密码错误）。',
+//   403: '用户得到授权，但是访问是被禁止的。',
+//   404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+//   405: '请求方法不被允许。',
+//   406: '请求的格式不可得。',
+//   410: '请求的资源被永久删除，且不会再得到的。',
+//   422: '当创建一个对象时，发生一个验证错误。',
+//   500: '服务器发生错误，请检查服务器。',
+//   502: '网关错误。',
+//   503: '服务不可用，服务器暂时过载或维护。',
+//   504: '网关超时。',
+// };
 
 /**
  * 异常处理程序
  */
 const errorHandler = (error: ResponseError) => {
-  const { response } = error;
+  // data是后端返回的异步结果
+  const { response, data } = error;
   if (response && response.status) {
-    const errorText = codeMessage[response.status] || response.statusText;
-    const { status, url } = response;
-
+    // const errorText = codeMessage[response.status] || response.statusText;
+    // const { status, url } = response;
+    const { status } = response;
     notification.error({
-      message: `请求错误 ${status}: ${url}`,
-      description: errorText,
+      // message: `请求错误 ${status}: ${url}`,
+      message: `请求错误 ${status}`,
+      description: data.errorMessage,
     });
   }
 
